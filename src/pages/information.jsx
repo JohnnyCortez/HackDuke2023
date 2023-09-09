0f20394b82254c78e0072a54b9581709784776a2
jaelyn cuellar <theebaddest@jaelyns-MacBook-Air.local>
2023-09-09 13:32:01 -0400

    initial - info page

--- src/pages/information.jsx
+++ src/pages/information.jsx
@@ -5,6 +5,133 @@ function Information() {
     return (
         <>
         <h2>Information</h2>
+
+        <h3> How to reduce your carbon footprint </h3>
+
+        <p> 
+        Reducing your carbon footprint is an important step towards mitigating climate change and minimizing your environmental impact. <br>
+        </br>
+        Here are several strategies and lifestyle changes you can consider to reduce your carbon footprint: <br></br>
+        <br></br>
+
+1. **Energy Efficiency:**
+<br></br>
+   - Use energy-efficient appliances and light bulbs.
+   - Properly insulate your home to reduce heating and cooling needs.
+   - Seal drafts around windows and doors.
+   - Set your thermostat to an energy-saving temperature.
+   - Unplug or turn off electronics when not in use.
+   - Consider installing solar panels to generate renewable energy.
+   <br></br>
+   <br></br>
+
+2. **Transportation:**
+<br></br>
+   - Use public transportation, carpool, or rideshare to reduce the number of individual vehicle trips.
+   - Choose fuel-efficient and low-emission vehicles if possible.
+   - Walk, bike, or use an electric scooter for short trips.
+   - Plan your errands to reduce the number of trips and use online shopping to consolidate deliveries.
+<br></br>
+<br></br>
+
+3. **Reduce, Reuse, Recycle:**
+<br></br>
+   - Reduce your consumption of single-use products and packaging.
+   - Reuse items whenever possible.
+   - Recycle paper, plastic, glass, and metal according to your local recycling program.
+   - Properly dispose of hazardous waste and electronics.
+   <br></br>
+   <br></br>
+4. **Water Conservation:**
+<br></br>
+   - Fix leaks and install water-saving fixtures in your home.
+   - Use a low-flow showerhead and faucet aerators.
+   - Collect rainwater for outdoor use.
+   - Only run the dishwasher and washing machine with full loads.
+
+   <br></br>
+   <br></br>
+
+5. **Food Choices:**
+<br></br>
+   - Reduce meat consumption, particularly red meat and processed meats.
+   - Choose locally sourced and seasonal foods to reduce transportation emissions.
+   - Minimize food waste by planning meals and storing food properly.
+   - Support sustainable farming practices.
+   <br></br>
+   <br></br>
+
+6. **Travel and Vacations:**
+<br></br>
+   - Consider eco-friendly travel options like trains or buses.
+   - Choose accommodations that prioritize sustainability.
+   - Offset your travel emissions by investing in carbon offset programs.
+   <br></br>
+   <br></br>
+
+7. **Composting:** 
+<br></br>
+Start composting organic waste like kitchen scraps and yard trimmings to reduce landfill methane emissions.
+<br></br>
+<br></br>
+
+8. **Reduce Air Travel:**
+<br></br>
+Air travel has a significant carbon footprint. Consider alternatives like video conferences for business meetings and vacation destinations closer to home.
+<br></br>
+<br></br>
+
+9. **Use Sustainable Products:** 
+<br></br>
+Support products and companies that prioritize sustainability, such as those using renewable materials, reducing waste, or practicing fair trade.
+<br></br>
+<br></br>
+
+10. **Advocate for Change:** 
+<br></br>
+Support policies and initiatives that promote sustainability and carbon reduction at the local, national, and global levels. Advocate for clean energy, conservation efforts, and sustainable transportation options.
+<br></br>
+<br></br>
+
+11. **Carbon Offsets:** 
+<br></br>
+Consider purchasing carbon offsets to compensate for your unavoidable emissions. These investments can fund projects like reforestation or renewable energy development.
+<br></br>
+<br></br>
+
+12. **Educate Yourself and Others:**
+<br></br>
+Stay informed about climate change and share information with friends and family to raise awareness.
+
+<br></br>
+<br></br>
+
+Reducing your carbon footprint is a gradual process that involves making sustainable choices in various aspects of your life. It's important to remember that every small change you make can contribute to a more sustainable and environmentally friendly lifestyle. Start with manageable steps, and over time, you can make a significant positive impact on the environment.
+
+</p>
+
+
+
+
+        <h3> Cleanups near me </h3>
+        <iframe
+        src='//www.epa.gov/sites/production/files/widgets/ef-cleanups.html'
+        id="cleanups"
+        width="225"
+        height="250"
+        scrolling="no"
+        frameborder="0"
+        marginwidth="3"
+        marginheight="0">
+        </iframe>
+
+
+        <h3> Offsetting videos </h3> {/* maybe have them play at front? */}
+
+
+
+
+
         </>
     )
 }
