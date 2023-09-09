f7066bfbe264c6b188d0d2cbce1f2fca79945ccd
Johnny Cortez <johnny.cortez@duke.edu>
2023-09-09 04:38:06 -0400

    set up account api supabase insert

--- client.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://rcnecovalbjxalrbqcxq.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjbmVjb3ZhbGJqeGFscmJxY3hxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQyNDQ4MTAsImV4cCI6MjAwOTgyMDgxMH0.gShIWXj517l-j9jlYhcRqBiH9ffTvU6AzZSBlA0Np_M"
-export const supabase = createClient(supabaseUrl, supabaseKey)
--- src/pages/createAccount.jsx
+++ src/pages/createAccount.jsx
@@ -1,10 +1,86 @@
 import { useEffect, useState } from "react";
+import { supabase } from "../client";
 
 function CreateAccount() {
+    const [username, setUsername] = useState(null)
+    const [country, setCountry] = useState(null)
+    const [city, setCity] = useState(null)
+    const [state, setState] = useState(null)
+    const [zipCode, setZipCode] = useState(null)
+    const [bio, setBio] = useState(null)
+    const [password, setPassword] = useState(null)
+
+
+    const addAccount = async () => {
+        await supabase
+          .from("accounts")
+          .insert({
+            username: username,
+            country: country,
+            city: city,
+            state: state,
+            zipCode: zipCode,
+            bio: bio,
+            password: password,
+          })
+          .select();
+        console.log("success");
+        window.location = "/";
+      };
+
+      function handleClick() {
+        addAccount();
+      }
 
     return (
         <>
         <h2>Create Account</h2>
+        <div class="form">
+          <label>User Name:</label>
+          <input
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+
+          <label>Password:</label>
+          <input
+            type="text"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <label>Bio:</label>
+          <input
+            type="text"
+            onChange={(e) => setBio(e.target.value)}
+           />
+
+          <label>Country:</label>
+          <input
+            type="text"
+            onChange={(e) => setCountry(e.target.value)}
+          />
+
+          <label>City:</label>
+          <input
+            type="text"
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <br />
+
+          <label>State:</label>
+          <input
+            type="text"
+            onChange={(e) => setState(e.target.value)}
+          />
+
+          <label>Zip Code:</label>
+          <input
+            type="text"
+            onChange={(e) => setZipCode(e.target.value)}
+          />
+          <button onClick={handleClick}>Submit</button>
+        </div>
+
         </>
     )
 }
